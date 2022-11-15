fffb474f43c92c9e32176c796d251fc444cca1d5
ramiz14 <ramizng@code.edu.az>
2022-11-15 18:28:49 +0400

    done

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
 var value= document.getElementById('ay')
 var btn=document.getElementById('btn')
 var result=document.getElementById('result')
+var sum=document.getElementById('sum')
 value.onchange = () =>{
     btn.onclick=function calc() {
         var mny = document.getElementById('mny').value
         var prc= document.getElementById('prc').value  
         var cem=(Number(mny)*Number(prc)/100)+Number(mny)
-        console.log(value.value);
+        sum.innerHTML=`Ümumi Borcunuz ${cem}`
         switch (Number(value.value)) {
             case 1:
-                result.innerHTML=(cem/6)
+                result.innerHTML=`Ayliq Odenis ${(cem/6) }`
                 break;
             case 2:
-                result.innerHTML=(cem/12)
+                result.innerHTML=`Ayliq Odenis ${(cem/12) }`
             break;
             case 3:
-                result.innerHTML=(cem/18)
+                result.innerHTML= `Ayliq Odenis ${(cem/18)}`
             break;
             case 4:
-                result.innerHTML=(cem/24)
+                result.innerHTML= `Ayliq Odenis ${(cem/24)}`
             break;
             case 5:
-                result.innerHTML=(cem/36)
+                result.innerHTML= `Ayliq Odenis ${(cem/36)}`
             break;
         
             default:
